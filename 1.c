1dd0585a4c4521e4f3a1eb9a80ae23b3e0f44b89
RAJKARTHI <31534375+RAJKARTHI@users.noreply.github.com>
2017-09-01 14:44:12 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -5,15 +5,15 @@ int main()
     int num;
    printf("Enter any num: ");
     scanf("%d", &num);
-    if(num > 0)
+    if(num > 1)
     {
         printf("Numb is POSITIVE");
     }
-    if(num < 0)
+    if(num < 1)
     {
         printf("Num is NEGATIVE");
     }
-    if(num == 0)
+    if(num == 1)
     {
         printf("Num is ZERO");
     }
